637660f559bc07ef82f363efe5507487c5e450ff
Ruddy Gobillard <ruddy.gobillard@etudiant.univ-reims.fr>
2025-01-15 18:45:05 +0100

    Ajout de la génération aléatoire

--- script/terrain_gen.gd
+++ script/terrain_gen.gd
@@ -1,8 +1,26 @@
 extends Node2D
 
 @onready var player: CharacterBody2D = $"../player"
+var rng = RandomNumberGenerator.new()
+var parcelleCount = 0
 
-# Note : Chaque parcelle est espacé de 570 sur l'axe x, l'axe y n'aura pas besoin d'être modifié
+func creerParcelle() -> void:
+	var randomNum = rng.randi_range(0,9)
+	var parcelleChoosen = "res://parcelle/sand_parcelle_template" + str(randomNum) + ".tscn"
+	
+	var parcelle = load(parcelleChoosen)
+	var instance = parcelle.instantiate()
+	parcelleCount += 1
+	instance.position.x += 1140 * parcelleCount
+	add_child(instance)
+
+func _ready() -> void:
+	creerParcelle()
+
+# Note : Chaque parcelle est espacé de 570 sur l'axe x, x2 = 1140, l'axe y n'aura pas besoin d'être modifié
 func _physics_process(_delta: float) -> void:
+	
 	if player.isMoving:
-		pass
+		await get_tree().create_timer(1).timeout
+		
+		creerParcelle()
